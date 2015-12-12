dae144cd23f9bee0404a2c5fb59ed0dd3ac639da
Adam Ruka <adamruka@amazon.com>
2015-12-13 00:25:40 +0100

    simple_queue_is_fifo_for_2_el - Green.

--- src/main/java/com/amazon/tdd_brownbag/SimpleQueue.java
+++ src/main/java/com/amazon/tdd_brownbag/SimpleQueue.java
@@ -3,20 +3,30 @@ package com.amazon.tdd_brownbag;
 import java.util.NoSuchElementException;
 
 public class SimpleQueue<T> {
-    private SimpleQueueNode<T> first;
+    private SimpleQueueNode<T> first, last;
 
     public void enqueue(T t) {
-        first = new SimpleQueueNode<>(t);
+        SimpleQueueNode<T> newNode = new SimpleQueueNode<>(t);
+        if (last != null) {
+            last.next = newNode;
+        } else {
+            first = newNode;
+        }
+        last = newNode;
     }
 
     public T dequeue() {
         if (first == null)
             throw new NoSuchElementException();
-        return first.value;
+
+        T result = first.value;
+        first = first.next;
+        return result;
     }
 
     private static final class SimpleQueueNode<E> {
         final E value;
+        SimpleQueueNode<E> next;
 
         public SimpleQueueNode(E value) {
             this.value = value;
