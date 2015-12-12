962348352d8992bd519d9937bf5d4e514081ab65
Adam Ruka <adamruka@amazon.com>
2015-12-12 22:43:29 +0100

    dequeue_on_empty_queue_throws_no_such_el - Green.

--- src/main/java/com/amazon/tdd_brownbag/SimpleQueue.java
+++ src/main/java/com/amazon/tdd_brownbag/SimpleQueue.java
@@ -1,7 +1,9 @@
 package com.amazon.tdd_brownbag;
 
+import java.util.NoSuchElementException;
+
 public class SimpleQueue<T> {
     public T dequeue() {
-        throw new UnsupportedOperationException();
+        throw new NoSuchElementException();
     }
 }
