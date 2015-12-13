53a781d5b07a9fb98e34f30a816ec70143a0fb3e
Adam Ruka <adamruka@amazon.com>
2015-12-13 01:36:36 +0100

    Refactoring to using a Queue implementation.

--- src/main/java/com/amazon/tdd_brownbag/SimpleQueue.java
+++ src/main/java/com/amazon/tdd_brownbag/SimpleQueue.java
@@ -1,36 +1,18 @@
 package com.amazon.tdd_brownbag;
 
+import java.util.LinkedList;
 import java.util.NoSuchElementException;
+import java.util.Queue;
 
 public class SimpleQueue<T> {
-    private SimpleQueueNode<T> first, last;
+    private Queue<T> queue = new LinkedList<>();
 
     public SimpleQueue<T> enqueue(T t) {
-        SimpleQueueNode<T> newNode = new SimpleQueueNode<>(t);
-        if (first != null) {
-            last.next = newNode;
-        } else {
-            first = newNode;
-        }
-        last = newNode;
+        queue.offer(t);
         return this;
     }
 
     public T dequeue() {
-        if (first == null)
-            throw new NoSuchElementException();
-
-        T result = first.value;
-        first = first.next;
-        return result;
-    }
-
-    private static final class SimpleQueueNode<E> {
-        final E value;
-        SimpleQueueNode<E> next;
-
-        public SimpleQueueNode(E value) {
-            this.value = value;
-        }
+        return queue.remove();
     }
 }
